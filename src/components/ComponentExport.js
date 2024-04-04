3d56bad0de0e01e97cb6cc980b530d7e706dcd21
Pranay dutta <rick15176@gmail.com>
2024-04-04 13:41:08 +0530

    fixed bugs & feat:create own Blog

--- src/components/ComponentExport.js
+++ src/components/ComponentExport.js
@@ -2,4 +2,4 @@ export { default as Blog } from "./Blog"
 export { default as Hero } from "./Hero"
 export { default as Navbar } from "./Navbar"
 export { default as News } from "./News"
-export { default as Modify } from "./Modify"
+
