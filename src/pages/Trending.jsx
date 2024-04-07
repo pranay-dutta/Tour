01e9386f583d5eb22588152e2559fe8d552b5a42
Pranay dutta <rick15176@gmail.com>
2024-04-07 15:48:22 +0530

    fixede error messaage  bug

--- src/pages/Trending.jsx
+++ src/pages/Trending.jsx
@@ -5,7 +5,7 @@ import { News, Navbar, Footer } from "../components/ComponentExport";
 
 const Trending = () => {
   const [results, setResults] = useState(null);
-  const [status, setStatus] = useState(null);
+  const [error, setError] = useState(null);
   const API_URL = import.meta.env.VITE_NEWS_API_URL;
   const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
   const styles = {
@@ -18,12 +18,11 @@ const Trending = () => {
   useEffect(() => {
     const fetchNews = async () => {
       try {
-        const { data, status } = await axios.get(`${API_URL}top-headlines?country=in&apiKey=${API_KEY}`);
+        const { data } = await axios.get(`${API_URL}top-headlines?country=in&apiKey=${API_KEY}`);
         setResults(data.articles);
-        setStatus(status);
       } catch (error) {
         setResults(null)
-
+        setError(true)
       }
     };
 
@@ -59,7 +58,7 @@ const Trending = () => {
             <h1 className={styles.heading}>Loading...</h1>
           }
         </div>
-        <p className="text-2xl text-center text-red-700">{ status > 400 && "Sorry this a free API. It doesn't takes https requests🥲"}</p>
+        <p className="text-2xl text-center text-red-700">{ error && "Sorry this a free API. It doesn't takes https requests🥲"}</p>
       </div>
       <Footer />
     </div>
